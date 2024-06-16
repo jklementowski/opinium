33fb8f738edb6a2e297261d437597dd572604bfe
jklementowski <janusz.klementowski@gmail.com>
2024-06-16 18:29:28 +0200

    add challange

--- fun5.py
+++ fun5.py
@@ -16,3 +16,27 @@ def GET_from_jsonplacholer(url: str):
     return resp_json
 
 GET_from_jsonplacholer(urljson)
+
+class simple_class:
+    """representing stack data strucuture"""
+    def __init__(self):
+        self.stack = []
+        print("Stack is: ",self.stack)
+        
+    def push_stack(self,number: int):
+        self.stack.append(number)
+        print("Pushed ", number)
+    
+    def pop_stack(self) -> int:
+        print("Popped last one")
+        return self.stack.pop()
+
+object_simple_class = simple_class()
+object_simple_class.push_stack(1)
+print("Current stack: ",object_simple_class.stack)
+object_simple_class.push_stack(2)
+object_simple_class.push_stack(3)
+object_simple_class.push_stack(4)
+print("Current stack: ",object_simple_class.stack)
+object_simple_class.pop_stack()
+print("Current stack: ",object_simple_class.stack)
